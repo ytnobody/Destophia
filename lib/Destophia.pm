a8a81e599675331a7529652e032bf672864bcd05
ytnobody / satoshi azuma <ytnobody@gmail.com>
2013-04-02 10:08:17 +0900

    add doc

--- lib/Destophia.pm
+++ lib/Destophia.pm
@@ -50,15 +50,73 @@ __END__
 
 =head1 NAME
 
-Destophia - 
+Destophia - More micro sized Web-API Framework
 
 =head1 SYNOPSIS
 
+In your API Root Class ...
+
+  package MyApp;
   use Destophia;
+  use DBI;
+  
+  ### Load config and instantiate Destopia
+  my $c = Destopia->new( config => +{
+      db   => +{ ... },
+      mail => +{ ... },
+  } );
+
+  ### Specify path-routing
+  $c->route(
+      qr'^/register$'   => 'MyApp::regiter';
+      qr'^/user/(\d+)$' => 'MyApp::user',
+      qr'^/$'           => 'MyApp::index',
+  );
+  
+  sub new { bless +{ c => $c }, 'MyApp' }
+  
+  ### store db connection into container
+  sub db {
+      my $self = shift;
+      my $db_conf = $self->c->config->{db};
+      $self->c->entry( db => sub { DBI->connect( @{$db_conf->{connect_info}} } );
+  }
+  
+  ### Controller logic
+  sub index {
+      my ($class, $self, $req) = @_;
+      return +{ hello => 'world'};
+  }
+  
+  sub user {
+      my ($class, $self, $req, $match) = @_;
+      my $userid = $match->{capture}[0];
+      my $sth = $self->db->prepare('SELECT * FROM user WHERE id = ?');
+      $sth->execute( $userid );
+      my $user = $sth->fetchrow_arrayref;
+      return +{ user  => $user };
+  }
+  
+  sub register {
+      my ($class, $self, $req) = @_;
+      my $name = $req->param('name');
+      my $age = $req->param('age');
+      my $sth = $self->db->prepare('INSERT INTO user (`name`,`age`) VALUES (?, ?)');
+      $sth->execute($name, $age);
+      return +{ done => 1 };
+  }
+  1;
+
+And, your app.psgi ...
+
+  use MyApp;
+  MyApp->app;
+
+Finally, you can plackup it.
 
 =head1 DESCRIPTION
 
-Destophia is
+Destophia is micro sized Web-API Framework.
 
 =head1 AUTHOR
 
